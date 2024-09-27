863984ab5af5d89fbeaa20a43773422af7af0323
GuerreroAguilarSergio <segueleo0305@gmail.com>
2024-09-26 20:03:54 -0600

    Código creado con MAVEN

---
+++ POO1_MAVEN/src/main/java/com/mycompany/poo1_maven/POO1_MAVEN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo1_maven;
+
+/**
+ *
+ * @author sergi
+ */
+public class POO1_MAVEN {
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola mundo!");
+    }
+}
